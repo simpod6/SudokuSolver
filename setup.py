7dea4c3a0f387efba9e5a16de136f8edddb85627
Simone Podico <simpod@gmail.com>
2019-12-27 01:10:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='SudokuSolver',
-      version='0.1dev',
-      description='A simple sudoku solver',
+      version='0.1',
+      description='A simple TDD python sudoku solver',
       url='tbd',
       author='Simone Podico',
       author_email='simpod@gmail.com',      
