15ba079980dca109fa59ddc5ef8f74cd18f8ed5b
Simone Podico <simpod@gmail.com>
2019-12-29 16:02:35 +0100

    Added bulk sudoku solution management.

--- SudokuSolver/main.py
+++ SudokuSolver/main.py
@@ -12,7 +12,25 @@ grid = ("004300209"
         "042910300" )
 
 
-def main():
+def main():    
+    with open('.\SudokuSolver\data\sudoku.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count > 0:
+                sudoku = Sudoku(row[0])            
+                sudoku.solve()
+                outputGrid = sudoku.getGrid()
+                if outputGrid == row[1]:
+                    print(f"Sudoku {line_count} solved!")
+                else:
+                    print(f"!!! Sudoku {line_count} NOT solved !!!")
+                del sudoku
+            line_count += 1
+        print(f'Processed {line_count} Sudokus')
+
+
+    """
     sudoku = Sudoku(grid)
     sudoku.solve()        
     
@@ -20,7 +38,7 @@ def main():
 
     for row in range(NUM_ROWS):
         print(outputGrid[row*NUM_COLS:row*NUM_COLS+9])
-
+    """
 
 if __name__ == "__main__":
     main()
